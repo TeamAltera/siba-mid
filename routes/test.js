872eb3e957314452229ad7a2836a6dc320414d0c
Altera520 <sencom1028@gmail.com>
2019-03-19 02:01:35 +0900

    use sequelize for DB mapping

--- routes/test.js
+++ routes/test.js
@@ -4,10 +4,18 @@ var router = express.Router();
 var models = require('../models');
 
 router.get('/register', function (req, res, next) {
-  models.hub.findAll().then(res=>{
-    console.log(res);
-  })
+    models.User.create({owner_id: 123, id:123})
+    .then(result => {
+        console.log('insert ok');
+    })
   res.json({ status: 'ok' });
 });
+
+router.get('/register/test', function (req, res, next) {
+    models.hub.findAll().then(res=>{
+      console.log(res);
+    })
+    res.json({ status: 'ok' });
+  });
 
 module.exports = router;
