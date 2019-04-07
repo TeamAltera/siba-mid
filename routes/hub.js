1b75625ade60aacb88f44bbe7e600ed6a60bae78
Altera520 <sencom1028@gmail.com>
2019-04-07 09:23:25 +0900

    add get hub information url

--- routes/hub.js
+++ routes/hub.js
@@ -2,15 +2,37 @@ var express = require('express');
 var querystring = require('querystring');
 var router = express.Router();
 var address = require('address');
-var ap = require('../services/ap-services');
+var network = require('network')
 var nrf24Service = require('../services/nrf24-services');
 var upnpService = require('../services/upnp-services');
+
+router.get('/', (req, res, next) => {
+  redisClient.get("isreg", (err, reply) => {
+    if (reply === 'N') {
+      network.get_public_ip((err, ip) => {
+        models.hub.findAll().then(hubInfo => {
+          res.json({
+            status: true,
+            ipv4: ip,
+            mac: hubInfo[0].mac
+          })
+        });
+      })
+    }
+    else
+      res.json({
+        status: false,
+        ipv4: '',
+        mac: ''
+      })
+  });
+})
 
 router.post('/', (req, res, next) => {
   //허브 등록
   const { headers } = req;
   let authorization = headers['authorization'];
-  authorization.replace('Bearer ','');
+  authorization.replace('Bearer ', '');
 
   console.log(`token: ${authorization}`)
 
@@ -24,14 +46,14 @@ router.post('/', (req, res, next) => {
   }
 
   //post redirect 수행
-  res.redirect( 307, 'http://localhost:8083/hub?'+querystring.stringify(data));
+  res.redirect(307, 'http://localhost:8083/hub?' + querystring.stringify(data));
 
 });
 
 router.get('/rf', function (req, res, next) {
   //명령이 수행 중에는 lock을 걸어야
   nrf24Service.broadcast();
-  res.json({status:'ok'});
+  res.json({ status: 'ok' });
 });
 
 module.exports = router;
